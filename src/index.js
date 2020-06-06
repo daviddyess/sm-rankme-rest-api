cc69928fb56dce25c906e663a7104179d4c4bfb3
daviddyess <david.dyess@gmail.com>
2020-06-06 12:57:42 -0600

    Added CORS support

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import cors from 'cors';
 import bodyParser from 'body-parser';
 import statRoutes from './routes/StatRoutes';
 import statusRoutes from './routes/StatusRoutes';
@@ -8,6 +9,7 @@ import Sequelize from 'sequelize';
 import config from './config';
 
 const app = express();
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
